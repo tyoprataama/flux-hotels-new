653b8010a5cfdad279b99dfdffab9fc2ccadd046
tyoprataama <tyoprataama004@gmail.com>
2023-12-31 14:52:29 +0700

    build: verificationToken schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,5 +3,6 @@ import booking from "./booking";
 import hotelRoom from "./hotelRoom";
 import review from "./review";
 import user from "./user";
+import verificationToken from "./verificationToken";
 
-export const schemaTypes = [user, account, booking, hotelRoom, review]
+export const schemaTypes = [user, account, booking, hotelRoom, review, verificationToken]
